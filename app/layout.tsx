a094a1f0f4dbd5c77b41d8b6d9ad940be03eaf17
Eche Codes <echeeze956@gmail.com>
2026-01-15 23:38:39 +0100

    fix: Adjust layout for better positioning and responsiveness of elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,12 +35,12 @@ export default function RootLayout({
           <div className="md:flex hidden">
             <Sidebar />
           </div>
-          <div className="md:pl-67.5 flex-1 flex flex-col min-h-screen">
+          <div className="md:pl-67.5 flex-1 flex flex-col h-screen ">
             <Header />
             <div className="bg-gray-300 rounded-t-2xl h-full sm:mr-5 mr-0 sm:p-8 p-4 sm:mt-0 mt-22 sm:min-h-[90vh] min-h-screen overflow-y-auto">
               {children}
             </div>
-            <div className="flex md:hidden absolute bottom-2 left-1/2 -translate-x-1/2">
+            <div className="flex md:hidden fixed bottom-2 left-1/2 -translate-x-1/2">
               <Mobilenav />
             </div>
           </div>
